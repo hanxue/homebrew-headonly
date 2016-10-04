1323413a11b0f88468199d57fdba8853e11c2f9f
Baptiste Fontaine <b@ptistefontaine.fr>
2016-10-04 23:40:45 +0200

    git-hg: Audit fixes

--- git-hg.rb
+++ git-hg.rb
@@ -1,25 +1,15 @@
-class HgInstalled < Requirement
-  satisfy { which "hg" }
-
-  def message; <<-EOS.undent
-    Mercurial is required to use this software.
-
-    You can install this with Homebrew using:
-      brew install mercurial
-
-    Or you can use an official installer from:
-      http://mercurial.selenic.com/
-    EOS
-  end
-end
-
 class GitHg < Formula
-  head "https://github.com/cosmin/git-hg.git"
+  desc "Scripts for tracking a Mercurial project from Git"
   homepage "http://offbytwo.com/git-hg/"
+  head "https://github.com/cosmin/git-hg.git"
 
-  depends_on HgInstalled
+  depends_on :hg
 
   def install
     prefix.install Dir["*"]
+  end
+
+  test do
+    shell_output(bin/"git-hg --help", 1)
   end
 end
