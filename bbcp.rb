5e234998785b7919627990759c8435d78d4953c0
Baptiste Fontaine <b@ptistefontaine.fr>
2016-10-04 23:32:26 +0200

    bbcp: Audit fixes

--- bbcp.rb
+++ bbcp.rb
@@ -1,11 +1,12 @@
 class Bbcp < Formula
+  desc "Point-to-point network file copy application"
   homepage "https://www.slac.stanford.edu/%7Eabh/bbcp"
   head "https://www.slac.stanford.edu/~abh/bbcp/bbcp.git"
 
   depends_on "openssl"
 
   def install
-    mkdir %w[bin obj]
+    %w[bin obj].each { |dir| (buildpath/dir).mkpath }
     system "make", "-C", "src", "Darwin"
     bin.install "bin/bbcp"
   end
