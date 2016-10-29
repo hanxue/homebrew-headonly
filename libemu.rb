5a9c29915e3fe4e99f13e3924a1a8550562ddfba
Baptiste Fontaine <b@ptistefontaine.fr>
2016-10-29 23:50:17 +0200

    libemu: audit fixes

--- libemu.rb
+++ libemu.rb
@@ -1,8 +1,11 @@
 class Libemu < Formula
+  desc "C library for basic x86 emulation and shellcode detection"
+  homepage "https://web.archive.org/web/20150812195102/http://libemu.carnivore.it/"
   head "http://git.carnivore.it/libemu.git"
-  homepage "http://libemu.carnivore.it/"
 
-  option "enable-python-bindings", "Compile bindings for Python"
+  option "with-python", "Compile bindings for Python"
+
+  deprecated_option "enable-python-bindings" => "with-python"
 
   depends_on "pkg-config" => :build
   depends_on "automake" => :build
@@ -18,9 +21,9 @@ class Libemu < Formula
       --disable-dependency-tracking
       --prefix=#{prefix}
     ]
-    args << "--enable-python-bindings" if build.include? "enable-python-bindings"
+    args << "--enable-python-bindings" if build.with? "python"
 
-    system "autoreconf -v -i"
+    system "autoreconf", "-v", "-i"
     system "./configure", *args
     system "make", "install"
   end
